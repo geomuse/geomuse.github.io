74448f8751c6ce23a20f6d89a9a9beb7ff7f0bff
geo <34396454+geomuse@users.noreply.github.com>
2025-02-25 12:07:28 +0800

    updated

---
+++ _posts/python/2025-02-27-python_project004.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: python 打印乘法表（九九乘法表） 专案
+date: 2025-02-27 10:24:29 +0800
+categories:
+    - python
+---
+
+...
--- _posts/python/project.py
+++ _posts/python/project.py
@@ -1,8 +1,16 @@
-import math 
+import random
 
-def match_even_or_odd(num):
+def match_num(num,count,real_num):
     match num :
-        case _ if num % 2 == 0 : return "even"
-        case _ : return "odd"
-
-print(match_even_or_odd(7))
+        case _ if num > real_num : 
+            return "太大了！"
+        case _ if num < real_num:
+            return "太小了！"
+        case real_num : 
+            return f"猜对了,{count}"
+print(real_num := random.randint(1, 100))        
+count = 0
+while True : 
+    num = float(input())
+    print(match_num(num,count,real_num))
+    count+=1
