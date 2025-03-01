834fbeedb874822b40ac84450f1a50868168b58d
geo <34396454+geomuse@users.noreply.github.com>
2025-03-01 22:01:51 +0800

    updated

--- _posts/na/2025-02-29-steffensen.md
+++ _posts/na/2025-02-29-steffensen.md
@@ -16,3 +16,22 @@ categories:
   };
 </script>
 <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+```py
+def steffensen(po,tol,no,f):
+    i = 1
+    while i <= no : 
+        p1 = f(po)
+        p2 = f(p1)
+        p = po - (p1-po)**2 / (p2-2*p1+po)
+        yield p
+        if abs(p-po) < tol :
+            return 
+        i+=1
+        po = p
+
+g = lambda x : (10/(x+4))**0.5
+
+for value in steffensen(10,1e-10,500,g):
+    print(value) 
+```
--- _posts/na/temp.py
+++ _posts/na/temp.py
@@ -62,7 +62,22 @@ def false_position(po,p1,tol,no,f):
             qo = q1
         p1=p
         q1=q
+
+def steffensen(po,tol,no,f):
+    i = 1
+    while i <= no : 
+        p1 = f(po)
+        p2 = f(p1)
+        p = po - (p1-po)**2 / (p2-2*p1+po)
+        yield p
+        if abs(p-po) < tol :
+            return 
+        i+=1
+        po = p
+
 f = lambda x : x**2 - x - 2
 
-for value in false_position(0,5,1e-10,500,f):
+g = lambda x : (10/(x+4))**0.5
+
+for value in steffensen(10,1e-10,500,g):
     print(value) 
