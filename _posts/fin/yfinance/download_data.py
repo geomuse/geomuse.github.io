0742bb0f66c0c3bafe55f5d00bc7d8a42d9186ee
geo <boonhong565059@gmail.com>
2026-01-30 21:29:28 +0800

    updated

--- _posts/fin/yfinance/download_data.py
+++ _posts/fin/yfinance/download_data.py
@@ -4,9 +4,9 @@ import yfinance as yf
 import os
 
 tickers = ['AAPL', 'MSFT', 'GOOG']
-data_dir = '/home/geo/Downloads/geo/_posts/fin/zipline/data/daily'
+data_dir = '/home/geo/Downloads/geo/_posts/fin/mt4/data/daily'
 os.makedirs(data_dir, exist_ok=True)
 
 for ticker in tickers:
-    df = yf.download(ticker, start='2020-01-01', end='2026-01-24')
+    df = yf.download(ticker, start='2020-01-01', end='2026-01-30')
     df.to_csv(os.path.join(data_dir, f'{ticker}.csv'))
